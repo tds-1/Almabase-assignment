5f0c5c7fdfbbd70b281d737c154391eef907f5fe
tds-1 <tanmaydeepsharma21@gmail.com>
2020-11-07 03:24:50 +0530

    readme added

---
+++ README.md
@@ -0,0 +1,62 @@
+# Tweepy Twitter Api
+[![Codacy Badge](https://api.codacy.com/project/badge/Grade/1ac554483fac462797ffa5a8b9adf2fa?style=flat-square)]()
+[![Build Status](https://api.travis-ci.org/fossasia/badgeyay.svg?branch=development&style=flat-square)]()
+
+Almabase Assignment
+  - Finding N most popular repositories of a given organization on Github base on number of forks.
+  - Finding M top committees and their commit counts of each repository.
+
+Link to the application - https://almabase-assignment-tds.herokuapp.com/
+
+## Pre-requisites (to run App locally)
+- Python 3.6 and pip should be preinstalled
+- Github Access token
+
+
+## Technology Stack
+
+- Programming Languages
+    - Python 3.6
+    
+- Frameworks
+  - Django 3.1.2
+
+- Frontend
+    - HTML 5
+    - CSS 3
+    - Jinja
+    - Bootstrap 4
+
+- APIs
+    - Github Api
+        
+
+## How To Use
+1. Clone the repository
+```sh
+git clone https://github.com/tds-1/Almabase-assignment
+```
+2. Generate **Github access token** from Github Developer console (https://github.com/settings/tokens) and paste them in the .env file along with the secret key. env file looks like below:
+```
+SECRET_KEY=SECRET_KEY
+TOKEN_ID=YOUR_TOKEN_ID
+
+```
+3. Create a **virtual environment**.
+4. Install the dependenies
+```sh
+pip install -r requirements.txt
+```
+5. **Run** the django server on local host.
+```sh 
+python manage.py runserver
+```
+
+
+## Snapshots
+- First page with which the interacts.
+![alt text](https://github.com/tds-1/Almabase-assignment/blob/master/images/homepage.png)
+- List of Repositories along with there information.
+![alt text](https://github.com/tds-1/Almabase-assignment/blob/master/images/repo.png)
+- List of top committees.
+![alt text](https://github.com/tds-1/Almabase-assignment/blob/master/images/users.png)
--- github/core.py
+++ github/core.py
@@ -18,49 +18,47 @@ class core:
     def logic(orgName, N, M):
         N = int(N)
         M = int(M)
-        #retrieving all the repositories for the received organisation
         repoList=[]
         topNRepoDict = {}
         topMCommitteesDict = {}
         pageNum=1
-        while(True):     #The result can consist of multiple pages, we loop until we reach the last page of our search
+        while(True):    
             request = requests.get(apiPrefix+'orgs/'+orgName+'/repos'+queryString+str(pageNum))
             json = request.json()
-            if(len(json) == 0):        #if there is no repo in json, it means that we have iterated over all the repos
+            if(len(json) == 0):        
                 break
             for repo in range(len(json)):
                 x = json[repo]
-                repoList.append((-x["forks_count"],x["name"], x["stargazers_count"], x["language"])) #storing no. of forks and repo name in repoList as tuples
+                repoList.append((x["forks_count"],x["name"], x["stargazers_count"], x["language"])) #storing no. of forks and repo name in repoList as tuples
             pageNum+=1
+        
+        repoList.sort(reverse=True)
 
-        heapq.heapify(repoList)   #creating a heap of the repoList
         repoCount = 0
-        while(repoCount<N and len(repoList)>0):     #popping top N repos from the heap
-            forkCount,repoName,star,language = heapq.heappop(repoList)
-            topNRepoDict[repoName] = (-forkCount,star,language)     #storing repoName as key and fork count as value in dictionary
-            topMCommitteesDict[repoName] = []       #storing repoName as key and empty list as value in 2nd dictionary
+        while(repoCount<min(N,len(repoList))):    
+            forkCount,repoName,star,language = repoList[repoCount]
+            topNRepoDict[repoName] = (forkCount,star,language)     
+            topMCommitteesDict[repoName] = []      
             repoCount+=1
 
-        #for each repo we will be retrieving all its committees and finding top M of them
         for repo in topMCommitteesDict:
-            committeesList = [] #to store committees for each repo
+            committeesList = [] 
             pageNum = 1
-            while(True):  #to iterate over all pages of search result
+            while(True):  
                 request = requests.get(apiPrefix+'repos/'+orgName+'/'+repo+'/contributors'+queryString+str(pageNum))
                 json = request.json()
                 if(len(json) == 0):
                     break
                 for contributor in range(len(json)):
                     x = json[contributor]
-                    committeesList.append((-x["contributions"],x["login"],x["avatar_url"],x["html_url"])) 
-                    #storing no. of commits and contributor name as tuple
+                    committeesList.append((x["contributions"],x["login"],x["avatar_url"],x["html_url"])) 
                 pageNum+=1
 
-            heapq.heapify(committeesList) #converting comitteesList to a heap
+            committeesList.sort(reverse=True)
             contributorCount = 0
-            while(contributorCount<M and len(committeesList)>0): #extracting top M contributors from the heap
-                contributionCount,contributorName,avtar,url=heapq.heappop(committeesList)
-                topMCommitteesDict[repo].append((contributorName,-contributionCount,avtar,url)) #storing tuple in the list for that particular repo
+            while(contributorCount<min(M,len(committeesList))): 
+                contributionCount,contributorName,avtar,url=committeesList[contributorCount]
+                topMCommitteesDict[repo].append((contributorName,contributionCount,avtar,url))
                 contributorCount+=1
 
         return (topNRepoDict,topMCommitteesDict)
