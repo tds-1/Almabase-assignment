4cd6b3fef75647455d453f981c557cba3ea87509
tds-1 <tanmaydeepsharma21@gmail.com>
2020-11-06 23:40:59 +0530

    whitenoise added

--- almabase/settings.py
+++ almabase/settings.py
@@ -136,7 +136,7 @@ STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'twitter_auth/static'),
+    os.path.join(BASE_DIR, 'github/static'),
 )
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
